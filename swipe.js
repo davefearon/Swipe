ba17df7ed9d998d084a78b90f216721ca629d4da
Dave Fearon <dave.fearon@gmail.com>
2013-05-17 15:21:54 -0700

    Changing the min swipe distance to 1/8th the width for a more natural feel on ios devices

--- swipe.js
+++ swipe.js
@@ -377,7 +377,7 @@ function Swipe(container, options) {
       var isValidSlide =
             Number(duration) < 250 &&            // if slide duration is less than 250ms
             Math.abs(delta.x) > 20 ||            // and if slide amt is greater than 20px
-            Math.abs(delta.x) > width / 2;       // or if slide amt is greater than half the width
+            Math.abs(delta.x) > width / 8;       // or if slide amt is greater than 1/8th the width
 
       // determine if slide attempt is past start and end
       var isPastBounds =
